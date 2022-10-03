7bc125320c15a548e9eed584f5ff949d4984174f
Andrew Okoro <okoroandrew09@gmail.com>
2022-10-03 12:04:15 -0400

    adding form and text area and backend

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+let user = {name: "Andrew", biography: "I am a professor in Engineering"}
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/profile', function(req, res, next) {
+  const name = req.body.name
+  const biography = req.body.biography
+  user = {name, biography}
+  res.status(200).json({message: "Success", user});
+});
+
+router.get('/profile', function(req, res, next) {
+  res.status(200).json({message: "Success", user});
 });
 
 module.exports = router;
